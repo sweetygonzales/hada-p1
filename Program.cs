9ed11568e378a84b5607d4f240f367ece91b6277
studschofl5035 <florian.schoner@stud.th-rosenheim.de>
2026-02-11 14:25:41 +0100

    Modified Program.cs to interact with user

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace hada_p1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            string choice;
+            do
+            {
+                Console.WriteLine("Enter unit (seconds/minutes):");
+                string unit = Console.ReadLine().Trim().ToLower();
+
+                Console.WriteLine("Enter value:");
+                double value = double.Parse(Console.ReadLine());
+
+                if (unit == "seconds")
+                    Console.WriteLine($"{value} seconds = {HadaP1.Seconds2Minutes(value)} minutes");
+                else if (unit == "minutes")
+                    Console.WriteLine($"{value} minutes = {HadaP1.Minutes2Seconds(value)} seconds");
+                else
+                    Console.WriteLine("Invalid unit");
+
+                Console.WriteLine("Do you want to continue? (y/n)");
+                choice = Console.ReadLine().Trim().ToLower();
+            } while (choice == "y");
         }
     }
 }
